8acfd0a9eb7952857398d7f638f4a44a3ca079b6
powe97 <116031952+powe97@users.noreply.github.com>
2024-06-03 13:07:50 -0400

    Import

--- migration/migrator/db.py
+++ migration/migrator/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import SmallInteger, String, TIMESTAMP
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
+from sqlalchemy.sql import text
 
 
 class Database:
